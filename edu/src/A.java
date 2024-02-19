2b589229db47fae5807425aa8d1c240a25e8d6c3
st-tomomi <tomomi.sato@excite-software.co.jp>
2024-02-19 17:30:00 +0900

    クラスとオブジェクト2

---
+++ edu/src/A.java
@@ -0,0 +1,28 @@
+public class A {
+    public static final String CONST_STRING1 = new String("定数1");
+    public final String CONST_STRING2 = new String("定数2");
+
+    // private static int staticValue;
+    // private int value;
+
+    // public A(int value) {
+    // this.value = value;
+    // staticValue = value;
+    // }
+
+    // public int getValue() {
+    // return value;
+    // }
+
+    // public int getStaticValue() {
+    // return staticValue;
+    // }
+
+    // public void setValue(int value) {
+    // this.value = value;
+    // }
+
+    // public void setStaticValue(int value) {
+    // staticValue = value;
+    // }
+}
--- edu/src/Main.java
+++ edu/src/Main.java
@@ -1,11 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
-        Parent a = new Child(1);
-        System.out.printf("a.value = %d\n", a.value);
-        System.out.printf("a.getValue() = %d\n", a.getValue());
-        System.out.printf("a.getValueString() = \"%s\"\n", a.getValueString());
-        System.out.printf("a.getValueString2() = \"%s\"\n", a.getValueString2());
-        System.out.printf(a.getValueString2());
+
+        A a1 = new A();
+        A a2 = new A();
+        System.out.printf("a1.CONST_STRING1 == a2.CONST_STRING1 : %b\n",
+                a1.CONST_STRING1 == a2.CONST_STRING1);
+        System.out.printf("a1.CONST_STRING2 == a2.CONST_STRING2 : %b\n",
+                a1.CONST_STRING2 == a2.CONST_STRING2);
+
+        // A a = new A(1);
+        // System.out.println("a初期化");
+        // System.out.printf("a.getValue() = %d\n", a.getValue());
+        // System.out.printf("a.getStaticValue() = %d\n", a.getStaticValue());
+        // A b = new A(3);
+        // System.out.println("b初期化");
+        // System.out.printf("a.getValue() = %d\n", a.getValue());
+        // System.out.printf("a.getStaticValue() = %d\n", a.getStaticValue());
+        // System.out.printf("b.getValue() = %d\n", b.getValue());
+        // System.out.printf("b.getStaticValue() = %d\n", b.getStaticValue());
+        // a.setValue(2);
+        // System.out.println("a.setValue(2)");
+        // a.setStaticValue(4);
+        // System.out.println("a.setStaticValue(4)");
+        // System.out.printf("a.getValue() = %d\n", a.getValue());
+        // System.out.printf("a.getStaticValue() = %d\n", a.getStaticValue());
+        // System.out.printf("b.getValue() = %d\n", b.getValue());
+        // System.out.printf("b.getStaticValue() = %d\n", b.getStaticValue());
     }
+
 }
