77feb59d001bf7472b2a644e530500de3e1735bb
st-tomomi <tomomi.sato@excite-software.co.jp>
2024-02-20 12:28:19 +0900

    コレクション1

--- edu/src/Main.java
+++ edu/src/Main.java
@@ -1,11 +1,23 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
     public static void main(String[] args) {
-        Parent a = new Child(1);
-        System.out.printf("a.value = %d\n", a.value);
-        System.out.printf("a.getValue() = %d\n", a.getValue());
-        System.out.printf("a.getValueString() = \"%s\"\n", a.getValueString());
-        System.out.printf("a.getValueString2() = \"%s\"\n", a.getValueString2());
-        System.out.printf(a.getValueString2());
+        List<String> list = new ArrayList<>();
+        list.add("1番目");
+        list.add("2番目");
+        list.add("3番目");
+
+        // 穴埋め部
+        // for文
+        System.out.print("【for文】");
+        String doublequotation = "\"";
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(doublequotation + list.get(i) + doublequotation);
+            System.out.println();
+        }
+
+        // 拡張for文
+        System.out.print("【拡張for文】");
     }
 }
