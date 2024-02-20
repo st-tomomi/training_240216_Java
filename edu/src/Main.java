73ee58c4a22c2bfbce2f6e993c16e55222ceca11
st-tomomi <tomomi.sato@excite-software.co.jp>
2024-02-20 13:02:54 +0900

    コレクション2

--- edu/src/Main.java
+++ edu/src/Main.java
@@ -1,11 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static final List<String> CONST_LIST;
+    static {
+        // 穴埋め部
+        CONST_LIST = new ArrayList<>();
+        CONST_LIST.add("1番目");
+        CONST_LIST.add("2番目");
+        CONST_LIST.add("3番目");
+    }
 
     public static void main(String[] args) {
-        Parent a = new Child(1);
-        System.out.printf("a.value = %d\n", a.value);
-        System.out.printf("a.getValue() = %d\n", a.getValue());
-        System.out.printf("a.getValueString() = \"%s\"\n", a.getValueString());
-        System.out.printf("a.getValueString2() = \"%s\"\n", a.getValueString2());
-        System.out.printf(a.getValueString2());
+        System.out.println("追加前:");
+        CONST_LIST.stream().forEach(e -> System.out.println(e));
+        CONST_LIST.add("4番目");
+        System.out.println("追加後:");
+        CONST_LIST.stream().forEach(e -> System.out.println(e));
     }
 }
