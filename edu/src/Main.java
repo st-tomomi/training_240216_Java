7dabbf6e585611039125651eea0d4311b25b6e67
st-tomomi <tomomi.sato@excite-software.co.jp>
2024-02-20 10:15:38 +0900

    クラスとオブジェクト3

--- edu/src/Main.java
+++ edu/src/Main.java
@@ -1,11 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-        Parent a = new Child(1);
-        System.out.printf("a.value = %d\n", a.value);
-        System.out.printf("a.getValue() = %d\n", a.getValue());
-        System.out.printf("a.getValueString() = \"%s\"\n", a.getValueString());
-        System.out.printf("a.getValueString2() = \"%s\"\n", a.getValueString2());
-        System.out.printf(a.getValueString2());
+        // int intA = 1;
+        // int intB = 1;
+        // System.out.printf("intA == intB : %b\n", intA == intB);
+        // Integer integerA = new Integer(1);
+        // Integer integerB = new Integer(1);
+        // System.out.printf("integerA == integerB : %b\n", integerA == integerB);
+        // String stringA = new String("1");
+        // String stringB = new String("1");
+        // System.out.printf("stringA == stringB : %b\n", stringA == stringB);
+
+        // System.out.printf("integerA.equals(integerB) : %b \n",
+        // integerA.equals(integerB));
+        // System.out.printf("stringA.equals(stringB) : %b \n",
+        // stringA.equals(stringB));
+
+        Integer integerA = Integer.valueOf(1);
+        Integer integerB = Integer.valueOf(1);
+        System.out.printf("integerA == integerB : %b\n", integerA == integerB);
+        String stringA = "1";
+        String stringB = "1";
+        System.out.printf("stringA == stringB : %b\n", stringA == stringB);
+
     }
+
 }
