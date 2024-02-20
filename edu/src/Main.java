15d73840062a84209cf0098baa4caf3e49b8d4d2
st-tomomi <tomomi.sato@excite-software.co.jp>
2024-02-20 16:18:20 +0900

    C

--- edu/src/Main.java
+++ edu/src/Main.java
@@ -1,10 +1,25 @@
+import java.util.*;
+
 public class Main {
     public static void main(String... args) throws Exception {
-        String text = "A";
-        int number = 1;
+        List<Long> results = new ArrayList<>();
 
-        // 穴埋め部
-        String displayText = String.format("text = \"%s\", number = %d", text, number);
-        System.out.println(displayText);
+        for (int i = 0; i < 10; i++) {
+
+            long start = System.nanoTime();
+
+            // 穴埋め部
+            String a = "";
+            for (int j = 0; j < 100000; j++) {
+                a += "B";
+            }
+
+            long end = System.nanoTime();
+            long result = end - start;
+            System.out.printf("実行時間(%d回目): %dナノ秒\n", i + 1, result);
+            results.add(result);
+        }
+
+        System.out.printf("実行時間(平均): %.0fナノ秒", results.stream().mapToLong(e -> e).average().orElse(Double.NaN));
     }
 }
