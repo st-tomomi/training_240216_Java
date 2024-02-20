f335c60e0cfecdfa3648f4ad49a299315fc4938b
st-tomomi <tomomi.sato@excite-software.co.jp>
2024-02-20 14:01:22 +0900

    switch文

--- edu/src/Main.java
+++ edu/src/Main.java
@@ -1,11 +1,31 @@
 public class Main {
 
     public static void main(String[] args) {
-        Parent a = new Child(1);
-        System.out.printf("a.value = %d\n", a.value);
-        System.out.printf("a.getValue() = %d\n", a.getValue());
-        System.out.printf("a.getValueString() = \"%s\"\n", a.getValueString());
-        System.out.printf("a.getValueString2() = \"%s\"\n", a.getValueString2());
-        System.out.printf(a.getValueString2());
+        System.out.println("【switch文】");
+        System.out.printf("A → %s\n", japaneseRank("A"));
+        System.out.printf("B → %s\n", japaneseRank("B"));
+        System.out.printf("C → %s\n", japaneseRank("C"));
+        System.out.printf("D → %s\n", japaneseRank("D"));
+        System.out.printf("F → %s\n", japaneseRank("F"));
+        System.out.printf("それ以外 → %s\n", japaneseRank("それ以外"));
     }
+
+    private static String japaneseRank(String rank) {
+        // 穴埋め
+        switch (rank) {
+            case "A":
+                return "秀";
+            case "B":
+                return "優";
+            case "C":
+                return "良";
+            case "D":
+                return "可";
+            case "F":
+                return "不可";
+            default:
+                return "不正な文字列です";
+        }
+    }
+
 }
