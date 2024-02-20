0ba9062df28f9feee0adf095bee7c4c55351e141
st-tomomi <tomomi.sato@excite-software.co.jp>
2024-02-20 16:02:38 +0900

    A

--- edu/src/Main.java
+++ edu/src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
+    public static void main(String... args) throws Exception {
+        String text = "A";
+        int number = 1;
 
-    public static void main(String[] args) {
-        Parent a = new Child(1);
-        System.out.printf("a.value = %d\n", a.value);
-        System.out.printf("a.getValue() = %d\n", a.getValue());
-        System.out.printf("a.getValueString() = \"%s\"\n", a.getValueString());
-        System.out.printf("a.getValueString2() = \"%s\"\n", a.getValueString2());
-        System.out.printf(a.getValueString2());
+        // 穴埋め部
+        String displayText = "text = \"" + text + "\", number = " + number;
+        System.out.println(displayText);
     }
 }
