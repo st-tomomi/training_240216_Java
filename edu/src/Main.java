ccdc5bc86606614ece265a9abfa8137a0bd851f1
st-tomomi <tomomi.sato@excite-software.co.jp>
2024-02-21 10:59:32 +0900

    文字列2

--- edu/src/Main.java
+++ edu/src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder text = new StringBuilder("a");
+        changeText(text);
 
-    public static void main(String[] args) {
-        Parent a = new Child(1);
-        System.out.printf("a.value = %d\n", a.value);
-        System.out.printf("a.getValue() = %d\n", a.getValue());
-        System.out.printf("a.getValueString() = \"%s\"\n", a.getValueString());
-        System.out.printf("a.getValueString2() = \"%s\"\n", a.getValueString2());
-        System.out.printf(a.getValueString2());
+        System.out.println(text.toString());
+    }
+
+    private static void changeText(StringBuilder text) {
+        text.append("b");
     }
 }
